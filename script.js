d0d7832ebf2d5692e8cc546bfe9a7629ad8af829
tyrakahn <4y5thtw9mf@privaterelay.appleid.com>
2026-09-17 15:35:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ const categories = [
     questions: [
       {
         value: 100,
-        question: "Vad kallas de kostnader i ett företag som inte ändras när försäljningsvolymen förändras?",
-        answer: "Fasta kostnader",
+        question: "De kostnader i ett företag som inte ändras när försäljningsvolymen förändras",
+        answer: "Vad är fasta kostnader?",
       },
       {
         value: 200,
-        question: "Vad kallas den skriftliga rapport som visar ett företags tillgångar, skulder och eget kapital vid ett visst datum (oftast sista december)?",
-        answer: "Balansräkning",
+        question: "Den skriftliga rapport som visar ett företags tillgångar, skulder och eget kapital vid ett visst datum (oftast sista december)",
+        answer: "Vad är en balansräkning",
       },
       {
         value: 300,
-        question: "Vad kallas det företagsekonomiska nyckeltal som visar hur stor andel av ett företags tillgångar som är finansierade med eget kapital?",
-        answer: "Soliditet",
+        question: "Det företagsekonomiska nyckeltal som visar hur stor andel av ett företags tillgångar som är finansierade med eget kapital",
+        answer: "Vad är soliditet",
       },
     ],
   },
@@ -27,18 +27,18 @@ const categories = [
     questions: [
       {
         value: 100,
-        question: "Under vilket årtionde invigdes Linköping Universitet som ett självständigt universitet?",
-        answer: "70-talet (1975)",
+        question: "Detta årtiondet invigdes Linköping Universitet som ett självständigt universitet",
+        answer: "När va 70-talet (1975)?",
       },
       {
         value: 200,
-        question: "Vilken plats har Linköping på listan över Sveriges största kommuner? (Befolkning)",
-        answer: "5:e plats",
+        question: "Denna plats är Linköping på listan över Sveriges största kommuner (Befolkning)",
+        answer: "Vad är 5:e plats?",
       },
       {
         value: 300,
-        question: "Under vilket århundrade började byggnationen av Linköpings domkyrka?",
-        answer: "1100-talet",
+        question: "Detta århundrade började byggnationen av Linköpings domkyrka",
+        answer: "När va 1100-talet?",
       },
     ],
   },
@@ -47,18 +47,18 @@ const categories = [
     questions: [
       {
         value: 100,
-        question: "I vilken ordning ska du använda besticken under en flerrätters middag?",
-        answer: "Utifrån och in",
+        question: "Denna ordning ska du använda besticken under en flerrätters middag",
+        answer: "Vad är utifrån och in?",
       },
       {
         value: 200,
-        question: "När du skålar på en formell bankett med din bordskamrat, var ska du titta enligt traditionell etikett?",
-        answer: "I ögonen (att titta ner i glaset eller bort anses oartigt)",
+        question: "När du skålar på en formell bankett med din bordskamrat, här ska du titta enligt traditionell etikett",
+        answer: "Vad är i ögonen (att titta ner i glaset eller bort anses oartigt)?",
       },
       {
         value: 300,
-        question: "Om din bordsgranne ber dig att räcka över saltet, vad måste du enligt klassisk vett och etikett alltid skicka med samtidigt?",
-        answer: "Peppern (salt och peppar betraktas som ett \"gift par\" och ska alltid skickas tillsammans på bordet)",
+        question: "Om din bordsgranne ber dig att räcka över saltet, detta måste du enligt klassisk vett och etikett alltid skicka med samtidigt",
+        answer: "Vad är pepparn? (Vad är salt och peppar betraktas som ett \"gift par\" och ska alltid skickas tillsammans på bordet)",
       },
     ],
   },
